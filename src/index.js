166227385d8b0ce7755fdcbce727fd6ab96821e2
Eli Craig <elidcraig@gmail.com>
2022-05-01 14:55:44 -0700

    All the changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+const newTaskDescription = document.getElementById('new-task-description')
+const prioritySelection = document.getElementById('priority-selection')
+const highPriority = document.getElementById('high-priority')
+const moderatePriority = document.getElementById('moderate-priority')
+const normalPriority = document.getElementById('normal-priority')
+const lowPriority = document.getElementById('low-priority')
+
+document.addEventListener('submit', event => {
+    event.preventDefault()
+    createItem()
+    newTaskDescription.value = ''
+})
+
+const createItem = () => {
+    let item = document.createElement('li')
+    let text = newTaskDescription.value
+    item.append(text)
+    styleAndAppendItem(item)
+}
+
+const styleAndAppendItem = item => {
+    if (prioritySelection.value === 'high') {
+      item.className = 'high'
+      highPriority.append(item)
+    } else if (prioritySelection.value === 'moderate') {
+      item.className = 'moderate'
+      moderatePriority.append(item)
+    } else if (prioritySelection.value === 'low') {
+      item.className = 'low'
+      lowPriority.append(item)
+    } else {
+      item.className = 'normal'
+      normalPriority.append(item)
+    }
+}
